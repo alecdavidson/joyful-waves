ddf1dad1d86473b050893fa5aabac3c2b3af4d18
alecdavidson <alecdavidson@nau.edu>
2015-01-19 14:28:24 -0700

    added base

---
+++ helloworld/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+url(r'^$', 'helloworld.views.home', name='home'),
+    # Examples:
+    # url(r'^$', 'helloworld.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
